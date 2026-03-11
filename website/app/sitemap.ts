53c6dfd5434e4702ec98fa3dcb2f0ffa58e2fc06
Eimantas Zalkauskas <eimantaszalkauskas@gmail.com>
2026-03-11 09:50:10 +0000

    updated sitemap

--- website/app/sitemap.ts
+++ website/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://clearstack.dev' // Update this to your actual domain
+  const baseUrl = 'https://www.clearstackconsulting.co.uk' // Update this to your actual domain
 
   return [
     {
